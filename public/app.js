cb90789311cee578e7181e0c494a711b3317fff4
umichdoe <johndoe@umich.edu>
2017-09-07 14:33:51 -0700

    express app with weather

---
+++ public/app.js
@@ -0,0 +1,44 @@
+console.log("Sanity Check: JS is working!");
+//
+$(document).ready(function(){
+
+  var apiKey = 'a6bb13ce7334f8d9eea9862a313100d0';
+  $.ajax({
+    method: 'GET',
+    url: `http://api.openweathermap.org/data/2.5/weather?zip=94597,us&units=imperial&appid=${apiKey}`,
+    success: onSuccess,
+    error: onError,
+  })
+
+function onSuccess(json){
+  console.log(json);
+  var tempFahrenheit = Math.ceil(json.main.temp);
+  $('.album-details').append(`<p>${tempFahrenheit}</p>`);
+  $('.album-details').append(`<p>${json.main.pressure}</p>`);
+}
+
+function onError(xhr, status, errorThrown) {
+   console.log('its an error');
+}
+
+ $.ajax({
+   method: 'GET',
+   url: 'http://localhost:3500/api/taquerias',
+   success: handleSuccessTaco,
+   error: handleErrorTaco
+ })
+
+function handleSuccessTaco(data) {
+  console.log(data[0].name);
+  data.forEach(function(item){
+    $('.taco-shops').append(`<p>${item.name}</p>`);
+  });
+}
+
+function handleErrorTaco(xhr, status, errorThrown) {
+   console.log('its an error');
+}
+
+
+
+}); //closes document dot ready
---
+++ server.js
@@ -0,0 +1,56 @@
+// server.js
+// SERVER-SIDE JAVASCRIPT
+var express = require('express');
+var app = express();
+
+  // Allow CORS: we'll use this today to reduce security so we can more easily test our code in the browser.
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+  // server.js
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.sendFile('views/index.html', {root: __dirname});
+  console.log(__dirname);
+})
+
+app.get('/api/albums', function(req, res) {
+  res.json(albums);
+})
+
+var albums = [
+    {
+      title: 'Cupid Deluxe',
+      artist: 'Blood Orange'
+    },
+    {
+      title: 'M3LL155X - EP',
+      artist: 'FKA twigs'
+    },
+    {
+      title: 'Fake History',
+      artist: 'letlive.'
+    }
+  ];
+
+app.get('/api/taquerias',function(req, res){
+  res.json(taquerias);
+})
+
+var taquerias = [
+      { name: "La Taqueria" },
+      { name: "El Farolito" },
+      { name: "Taqueria Cancun" }
+    ];  
+
+
+
+
+//connects to local host or production port
+app.listen(process.env.PORT || 3500, function() {
+  console.log('Listening on port 3500');
+})
